21c328ee2b60fb2accbe326ddae500161ae555d7
nailed1 <max20070608@gmail.com>
2026-03-04 15:54:19 +0800

    added pinger.py, parses datas (last avaliable one)

---
+++ pinger/pinger.py
@@ -0,0 +1,17 @@
+import requests
+from datetime import datetime, timedelta
+
+url_template="https://simurg.space/gen_file?data=obs&date={date}"
+current=datetime.now()
+while True:
+    date=current.strftime("%Y-%m-%d")
+    url=url_template.format(date=date)
+
+    response=requests.get(url=url_template,stream=True)
+    print(f"For {date} got: ",response)
+
+    if response.status_code == 200:
+        print(f"Last avaliable data are for {date}")
+        break
+    else:
+        current = current -timedelta(days=1)

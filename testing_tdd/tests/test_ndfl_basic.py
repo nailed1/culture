063bb38b487ff3ecc0d704f373949f2e2507e847
nailed1 <max20070608@gmail.com>
2026-03-10 16:31:56 +0800

    added a few more tests

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -1,4 +1,16 @@
 from ndfl import calculate_tax
 
 def test_basic_ndfl():
-    assert calculate_tax(200_000) == 20_000
+    assert calculate_tax(200_000) == 26_000
+
+def test_ndfl_tier_2():
+    assert calculate_tax(3000_000) == 402_000
+
+def test_ndfl_tier_3():
+    assert calculate_tax(7_000_000) == 1_062_000
+
+def test_ndfl_tier_4():
+    assert calculate_tax(30_000_000) == 5_402_000
+
+def test_ndfl_tier_5():
+    assert calculate_tax(70_000_000) == 13_802_000

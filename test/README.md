5f12dd13c4d9b55b1a33394b749c1828d1c143f1
nailed1 <max20070608@gmail.com>
2026-03-24 17:54:12 +0800

    did a new build with different name, uploaded library on test.pypi

---
+++ test/README.md
@@ -0,0 +1,13 @@
+# ndfl
+
+Python-пакет для работы с НДФЛ.
+
+## Установка
+
+```bash
+pip install ndfl-nailed1
+```
+
+## Репозиторий
+
+Исходный код доступен на GitHub: [https://github.com/nailed1/culture/tree/feature/makeutil/test](https://github.com/nailed1/culture/tree/feature/makeutil/test)
--- test/setup.py
+++ test/setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(
-    name = "ndfl",
-    version = "0.0.0",
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-    package_dir = {"": "src"},
-    packages = find_packages(where="src")
+setup(
+    name="ndfl-nailed1",
+    version="0.0.0",
+    author="nailed1",
+    description="Python-пакет для работы с НДФЛ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nailed1/culture/tree/feature/makeutil/test",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )

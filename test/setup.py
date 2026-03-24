4bf7607335fc5658e6b4ec722142270c82086ef1
nailed1 <max20070608@gmail.com>
2026-03-24 16:09:59 +0800

    did a build for an ndfl library, edited setup.py (fixed)

--- test/setup.py
+++ test/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test-package",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    name = "ndfl",
+    version = "0.0.0",
+
+    package_dir = {"": "src"},
+    packages = find_packages(where="src")
 )
